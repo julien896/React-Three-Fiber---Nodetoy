63306dbc46aafd3c62e2a04f07006369422da9c5
Julian <julicogui@gmail.com>
2023-06-13 14:36:26 -0300

    feat:scene elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import { Environment, OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import Scene from "./Scene";
+import { Suspense, useEffect } from "react";
+import { useThree } from "@react-three/fiber";
 
 function App() {
+  const state = useThree();
+  
+  useEffect(() => {
+    state.gl.toneMappingExposure = 5;
+  }, [state.gl]);
+
   return (
     <>
       <Environment
@@ -15,6 +24,10 @@ function App() {
 
       <PerspectiveCamera makeDefault fov={33} position={[-0.07, 16.41, -24.1]} />
       <OrbitControls target={[0.02, 0.806, 0.427]} maxPolarAngle={Math.PI * 0.45} />
+
+      <Suspense fallback={null}>
+        <Scene />
+      </Suspense>
     </>
   )
 }
