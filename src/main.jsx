2cf3e2f8b1d4da119f3ef6681526b03dbe53d29a
Julian <julicogui@gmail.com>
2023-06-13 13:09:47 -0300

    feat: envmaps

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,4 @@
+import { Canvas } from '@react-three/fiber';
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
@@ -5,6 +6,8 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Canvas>
+      <App />
+    </Canvas>
   </React.StrictMode>,
 )
